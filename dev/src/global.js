f62b049f5e0cf12b0dc11c55d749fc036c8cb642
Grigoriy-Chokie <100275730+Grigoriy-Chokie@users.noreply.github.com>
2025-11-20 12:34:10 +0200

    init dev structure

---
+++ dev/src/global.js
@@ -0,0 +1,36 @@
+ // modules
+window.PubSub = require("./scripts/pubsub");
+
+window.debounce = function (fn, wait) {
+  let t;
+  return (...args) => {
+    clearTimeout(t);
+    t = setTimeout(() => fn.apply(this, args), wait);
+  };
+};
+
+window.throttle = function (cb, delay) {
+  let wait = false;
+  let storedArgs = null;
+
+  function checkStoredArgs() {
+    if (storedArgs == null) {
+      wait = false;
+    } else {
+      cb(...storedArgs);
+      storedArgs = null;
+      setTimeout(checkStoredArgs, delay);
+    }
+  }
+
+  return (...args) => {
+    if (wait) {
+      storedArgs = args;
+      return;
+    }
+
+    cb(...args);
+    wait = true;
+    setTimeout(checkStoredArgs, delay);
+  };
+};
